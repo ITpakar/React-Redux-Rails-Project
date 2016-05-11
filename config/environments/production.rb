344246f58b0621dadbc8b9b17b7035902002d294
betop <stidpeace@gmail.com>
2016-05-11 06:45:08 +0300

    use secrets.yml for production env var setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = Rails.application.secrets[:rails_serve_static_files]
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
     end
     resources :notifications
     resources :comments
-    resources :document_signers
-    resources :documents
+    resources :documents do
+      resources :document_signers
+    end
     resources :folders
     resources :tasks
     resources :categories
@@ -22,8 +23,17 @@ Rails.application.routes.draw do
                   sessions: "sessions",
                   registrations: "registrations"
                 }
-
   end
+  get 'signin',      to: 'home#signin'
+  get 'signup',      to: 'home#signup'
+  get 'deals',       to: 'home#deals'
+  get 'deals/:id',   to: 'home#deal'
+  get 'team',        to: 'home#team'
+  get 'team-item',   to: 'home#team_item'
+  get 'deal-client', to: "home#deal_client"
+  get 'deal-file',   to: "home#deal_file"
+  get 'report',      to: "home#report"
+  get 'setting',     to: "home#setting"
 
-  root to: "home#index", as: :home
+  root to: "home#index"
 end
