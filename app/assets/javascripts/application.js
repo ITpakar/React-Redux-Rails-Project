4c5f4813f6d047e2aa0e2b0b03fc8147b6a6b828
Chintan Parikh <chintan@pennywhale.com>
2016-05-24 16:22:18 +0800

    All assets working again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,25 +6,6 @@
 //= require vendor-bundle
 //= require app-bundle
 
-window.$ = window.jQuery = require('jquery');
-require('jquery-ujs');
-// require('moment');
-
-require('./plugins/bootstrap');
-require('./plugins/bootstrap.file-input');
-require('./plugins/bootstrap-select');
-require('./plugins/equal-height');
-require('./plugins/fastclick');
-require('./plugins/icheck.min');
-require('./plugins/jquery.peity');
-require('./plugins/jquery.sparkline.min');
-require('./plugins/modernizr');
-require('./plugins/morris');
-require('./plugins/placeholders');
-require('./plugins/moment');
-require('./plugins/bootstrap-datetimepicker.min');
-require('./custom/scripts');
-
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
