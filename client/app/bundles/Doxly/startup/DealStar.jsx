d74170d3d67422dcef0d820b796cd1f101b5bf78
Chintan Parikh <chintan@pennywhale.com>
2016-05-27 17:50:35 +0800

    Fix commentbox placement

---
+++ client/app/bundles/Doxly/startup/DealStar.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+
+// import DealStar from '../components/DealStar';
+
+// export default (props) => {
+//   const store = ReactOnRails.getStore('doxlyStore');
+//   const reactComponent = {
+//     <Provider store={store}
+//   }
+// }
