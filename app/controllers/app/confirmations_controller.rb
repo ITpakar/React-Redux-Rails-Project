e593aa48fd47dd1556863347af24f5b68908e064
andikabijaya <it_pakar@andikabijayas-Mac.local>
2016-06-08 10:25:51 +0800

    organization deals

--- app/controllers/app/confirmations_controller.rb
+++ app/controllers/app/confirmations_controller.rb
@@ -1,2 +1,48 @@
 class App::ConfirmationsController < Devise::ConfirmationsController
+  # GET /resource/confirmation/new
+  def new
+    super
+  end
+
+  # POST /resource/confirmation
+  def create
+    super
+  end
+
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      DealCollaboratorInvite.where(email: resource.email).each do |invite|
+        invite.deal.add_collaborator!(resource.organization_user, invite.addor)
+      end
+      DealCollaboratorInvite.where(email: resource.email).delete_all
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+
+  protected
+
+    # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
+    end
+
+    # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
+    end
+
+    def translation_scope
+      'devise.confirmations'
+    end
 end
