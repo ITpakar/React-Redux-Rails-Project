272ac42f303b405aec0c1cfe4dd4455a758d5cd4
andikabijaya <it_pakar@andikabijayas-Mac.local>
2016-06-17 22:25:38 +0800

    set version automaticall

--- app/models/deal_document_version.rb
+++ app/models/deal_document_version.rb
@@ -2,6 +2,15 @@ class DealDocumentVersion < ApplicationRecord
   include Traversable
   belongs_to :deal_document
 
+  before_create :set_name
+
+  def set_name
+    self.deal_document.versions.order('created_at DESC').each do |version|
+      self.name = (version.name.to_f + 1).to_s
+      break
+    end
+  end
+
   def to_hash
     data = {
       id:               self.id,
