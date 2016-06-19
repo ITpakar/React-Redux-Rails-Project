e762bd50dffdff075cec70a731ab19e9048edf07
Chintan Parikh <chintan@pennywhale.com>
2016-06-19 16:52:31 -0400

    Define default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,6 @@ Rails.application.configure do
     :authentication  => :plain
   }
 
-  config.default_url_options[:host] = 'http://72.14.176.22:3000/'
+  Rails.application.routes.default_url_options[:host] = 'http://72.14.176.22:3000/'
 
 end
