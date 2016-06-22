f1c5a6fb1683bb330efa3e4a3d1a547b07105225
SamTN <samtranngocqb@yahoo.com>
2016-06-22 10:38:57 +0700

    Finish closing_book_spec.

--- lib/factories/closing_book.rb
+++ lib/factories/closing_book.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :closing_book do
     status                   { ClosingBook::STATUSES.sample }
     index_type               {ClosingBook::INDEX_TYPES.sample}
-    url                      {FFaker::Internet.http_url}
   end
 end
